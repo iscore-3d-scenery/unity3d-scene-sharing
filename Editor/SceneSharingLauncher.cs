ce7abc67ed8ed8e516563792d6b78d5c2b43b733
Théo CHASSAIGNE <hixe@hixe.fr>
2017-01-09 15:29:10 +0100

    Working export

---
+++ Editor/SceneSharingLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// SceneSharing Manager, to perform an Update to each SceneSharing object at each frame
+
+[InitializeOnLoad]
+class SceneSharingLauncher
+{
+    static List<SceneSharing> scenesList = new List<SceneSharing>();
+    static SceneSharingLauncher()
+    {
+        EditorApplication.update += SceneSharingLauncher.Update;
+    }
+
+    public static void Update()
+    {
+        foreach (SceneSharing s in scenesList)
+            s.UpdateMe();
+    }
+    
+    public static void addScene(SceneSharing s)
+    {
+        scenesList.Add(s);
+        s.Init(new Preset());
+    }
+}
